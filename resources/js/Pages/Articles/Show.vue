d68a878c5153e02095db1ad6a94d21a8a8fdfa8a
Jasper Frumau <jasper@imagewize.com>
2023-04-26 09:59:03 +0700

    v-html for html parsing

--- resources/js/Pages/Articles/Show.vue
+++ resources/js/Pages/Articles/Show.vue
@@ -23,8 +23,7 @@ const form = useForm({
          </h2>
          <div class="py-12">
              <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                     {{ article.body }}
+                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg" v-html="article.body">
                  </div>
              </div>
          </div>
