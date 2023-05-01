6510af8e03b31d9e0716115096ea76f01273f3e8
Jasper Frumau <jasper@imagewize.com>
2023-05-01 12:00:41 +0700

    article header display patch

--- resources/js/Pages/Articles/Show.vue
+++ resources/js/Pages/Articles/Show.vue
@@ -21,12 +21,13 @@ const form = useForm({
          <h2 class="font-semibold text-xl text-gray-800 leading-tight">
              {{ article.title }}
          </h2>
+    </template>
+
          <div class="py-12">
              <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                  <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5" v-html="article.body">
                  </div>
              </div>
          </div>
-     </template>
   </app-layout>
 </template>
